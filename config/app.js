f43da1f60a5f8818b9be5d99a333c4e07351748b
Mateus Chagas mateus.sousa@miraeducacao.com.br <mateuschs@gmail.com>
2017-10-27 03:08:16 -0200

    fixing config

--- config/app.js
+++ config/app.js
@@ -1,12 +1,13 @@
 
-const API_BASE_PATH = '';
+const API_BASE_PATH = 'http://localhost:3000';
 
 
 module.exports = {
     api: {
         REST: {
             paths:  {
-                base: API_BASE_PATH
+                base: API_BASE_PATH,
+                dimensions: '/dimensions'
             },
             timeout: 10000
         }
